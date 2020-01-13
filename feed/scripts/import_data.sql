075f20ebefde5dd932304d64dbf14d5b2e9288f6
ivyna santino <ivyna.alves@ccc.ufcg.edu.br>
2020-01-13 14:57:59 -0300

    adiciona tabela de contratos

--- feed/scripts/import_data.sql
+++ feed/scripts/import_data.sql
@@ -1,5 +1,6 @@
 \copy licitacao FROM '/data/licitacoes/2019/licitacao.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
 
+ALTER TABLE contrato ADD CONSTRAINT fk_contrato FOREIGN KEY(CD_ORGAO, NR_LICITACAO, ANO_LICITACAO, CD_TIPO_MODALIDADE) REFERENCES licitacao(CD_ORGAO, NR_LICITACAO, ANO_LICITACAO, CD_TIPO_MODALIDADE);
 \copy contrato FROM '/data/contratos/2019/contrato.csv' WITH NULL AS '' DELIMITER ',' CSV HEADER;
 
 ALTER TABLE empenho DROP CONSTRAINT empenho_pkey;
